233ee6301a13ae15980f4c92917bdb3839d864c0
Sergio Rubio <rubiojr@frameos.org>
2020-05-27 14:31:34 +0200

    Install docker-compose

--- docker.sh
+++ docker.sh
@@ -15,3 +15,6 @@ sunzi.mute add-apt-repository --yes \
 sunzi.mute apt-get update
 
 sunzi.install docker-ce docker-ce-cli containerd.io docker-compose
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
