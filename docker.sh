60edb0ffbdb3a36711a776d8922b5cf82428d4e1
Sergio Rubio <rubiojr@frameos.org>
2020-05-27 14:35:35 +0200

    docker recipe tweaks

--- docker.sh
+++ docker.sh
@@ -16,5 +16,5 @@ sunzi.mute apt-get update
 
 sunzi.install docker-ce docker-ce-cli containerd.io
 
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sunzi.mute curl -s -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sunzi.mute chmod +x /usr/local/bin/docker-compose
