360098ce57373098c0c1d7e62c2d650465d75076
Sergio Rubio <rubiojr@frameos.org>
2020-06-04 20:32:40 +0200

    Add some safety

--- docker.sh
+++ docker.sh
@@ -1,23 +1,20 @@
 main() {
-  arch=$(uname -m)
-  march=$(echo $arch | sed s/armv7l/armhf/ | sed s/x86_64/amd64/)
-  if [ "$arch" != x86_64 ] && [ "$arch" != armv7l ]; then
-   echo "syncthing: architecture not supported" >&2
-   return
-  fi
+  # abort if docker already installed
+  which docker 2>/dev/null && return
+
   sunzi.mute apt-get remove --yes --purge docker docker-engine docker.io containerd runc
-  sunzi.install apt-transport-https \
-                ca-certificates \
-                curl \
-                gnupg-agent \
-                software-properties-common
 
   curl -sSL https://get.docker.com | sh
 
-  sunzi.mute curl -s -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sunzi.mute chmod +x /usr/local/bin/docker-compose
   if [ -n "$SUDO_USER" ]; then
     sunzi.mute gpasswd -a $SUDO_USER docker
+  fi
+
+  if [ "$arch" = "x86_64" ]; then
+    sunzi.mute curl -s -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sunzi.mute chmod +x /usr/local/bin/docker-compose
+  else
+    echo "docker-compose install not supported in non x86_64 architectures" >&2
   fi
 }
 
